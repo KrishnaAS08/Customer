6218554a33740535035f944f57dbeeae756366d3
674228 <674228@cognizant.com>
2019-05-17 19:09:05 +0530

    initial commit

---
+++ BackEnd/Customer/src/main/java/com/cognizant/Customer/repo/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.cognizant.Customer.repo;
+
+import com.cognizant.Customer.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+
+    List<Customer> findByAge(int age);
+}
---
+++ FrontEnd/Customer/src/app/customer.ts
@@ -0,0 +1,6 @@
+export class Customer {
+    id: number;
+    name: String;
+    age: number;
+    active: boolean;
+}
